f40732fe1bbd8dcd696d7692125f55ea48c1f6bf
Severn Everett <severn.everett@gmail.com>
2021-11-24 14:14:19 +0100

    Created basic UI layout

--- src/main/kotlin/com/severett/kotlincalculator/main.kt
+++ src/main/kotlin/com/severett/kotlincalculator/main.kt
@@ -1,8 +1,6 @@
 package com.severett.kotlincalculator
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -12,50 +10,103 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import com.severett.kotlincalculator.model.Operation
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Kotlin Calculator",
-        state = rememberWindowState(width = 400.dp, height = 300.dp)
+        state = rememberWindowState(width = 300.dp, height = 350.dp),
+        resizable = false,
     ) {
         // val count = remember { mutableStateOf(0) }
         MaterialTheme {
-            Row {
-                // Buttons 1, 4, and 7
-                Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
+            Column {
+                Row(modifier = Modifier.padding(vertical = 5.dp)) {
+                    Text(text = "0")
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(Operation.MODULO.symbol)
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("CE")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("C")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("⌫")
+                    }
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("x⁻¹")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("x²")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("√")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(Operation.ADD.symbol)
+                    }
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
                         Text("1")
                     }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
-                        Text("4")
-                    }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
-                        Text("7")
-                    }
-                }
-                // Buttons 2, 5, and 8
-                Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
                         Text("2")
                     }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
-                        Text("5")
-                    }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
-                        Text("8")
-                    }
-                }
-                // Buttons 3, 6, and 9
-                Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
                         Text("3")
                     }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(Operation.SUBTRACT.symbol)
+                    }
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("4")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("5")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
                         Text("6")
                     }
-                    Button(modifier = Modifier.align(Alignment.CenterHorizontally), onClick = {}) {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(Operation.MULTIPLY.symbol)
+                    }
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("7")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("8")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
                         Text("9")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(Operation.DIVIDE.symbol)
+                    }
+                }
+                Row {
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("±")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("0")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text(".")
+                    }
+                    Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+                        Text("=")
                     }
                 }
             }
---
+++ src/main/kotlin/com/severett/kotlincalculator/model/Operation.kt
@@ -0,0 +1,9 @@
+package com.severett.kotlincalculator.model
+
+enum class Operation(val symbol: String) {
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("×"),
+    DIVIDE("÷"),
+    MODULO("%"),
+}
